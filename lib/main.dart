3c35287ea7d1ef67742350f8c9af6b52864b5d04
Vikas Kumar <46410948+detroitcr@users.noreply.github.com>
2022-06-16 09:54:02 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:api/Screen/home_screen.dart';
+import 'package:flutter/material.dart';
+  import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  
+// ...
+
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+
+    
+      home:  HomeScreen(),
+    );
+  }
+}
+
---
+++ lib/utils/assets.dart
@@ -0,0 +1,30 @@
+class Assets{
+  Assets._();
+  static const String clear = 'assets/images/clear.png';
+  static const String cloud = 'assets/images/cloud.png';
+  static const String cloudy = 'assets/images/cloudy.png';
+  static const String fog = 'assets/images/fog.png';
+  static const String heavycloudy = 'assets/images/heavycloudy.png';
+  static const String heavyrain = 'assets/images/heavyrain.png';
+  static const String humidity = 'assets/images/humidity.png';
+  static const String lightdrizzle = 'assets/images/lightdrizzle.png';
+  static const String lightrain = 'assets/images/lightrain.png';
+  static const String lightrainshower = 'assets/images/lightrainshower.png';
+  static const String menu = 'assets/images/menu.png';
+  static const String mist = 'assets/images/mist.png';
+  static const String rainHeavyShower = 'assets/images/moderateorheavyrainshower.png';
+  static const String rainHeavyThunder = 'assets/images/moderateorheavyrainwiththunder.png';
+  static const String moderaterain = 'assets/images/moderaterain.png';
+  static const String moderaterainTime = 'assets/images/moderaterainattimes.png';
+  static const String overcast = 'assets/images/overcast.png';
+  static const String partlyCloud = 'assets/images/partlycloudy.png';
+  static const String patchdrizzle = 'assets/images/patchylightdrizzle.png';
+  static const String patchLightrain = 'assets/images/patchylightrain.png';
+  static const String patchyRainThunder = 'assets/images/patchylightrainwiththunder.png';
+  static const String patchyRainpossible = 'assets/images/patchyrainpossible.png';
+  static const String pin = 'assets/images/pin.png';
+  static const String profile = 'assets/images/profile.JPG';
+  static const String sunny = 'assets/images/sunny.png';
+  static const String thunderBreak = 'assets/images/thunderyoutbreakspossible.png';
+  static const String windSpeed = 'assets/images/windspeed.png';
+}
